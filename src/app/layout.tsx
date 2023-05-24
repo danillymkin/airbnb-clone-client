cf5994e7580fda0ccdca5aa9561ea7a177b6dc3c
danillyamkin <danilwebdev@gmail.com>
2023-05-25 00:59:10 +0300

    Changed font to nunito

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
 import React from 'react';
+import { Nunito } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+import './globals.css';
+
+const nunito = Nunito({ subsets: ['latin', 'cyrillic'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={nunito.className}>{children}</body>
     </html>
   );
 }
