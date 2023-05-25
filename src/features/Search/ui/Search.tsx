63238fe6f701c0500e77be23ded994c9059947b4
danillyamkin <danilwebdev@gmail.com>
2023-05-26 02:38:10 +0300

    feat(search): create search component

---
+++ src/features/Search/ui/Search.tsx
@@ -0,0 +1,36 @@
+import React, { FunctionComponent } from 'react';
+import { BiSearch } from 'react-icons/bi';
+
+interface SearchProps {}
+
+type Props = SearchProps;
+
+export const Search: FunctionComponent<Props> = ({}): JSX.Element => {
+  return (
+    <div
+      className={
+        'w-full cursor-pointer rounded-full border py-2 shadow-sm transition hover:shadow-md md:w-auto'
+      }
+      role={'search'}
+    >
+      <div className={'flex items-center justify-between'}>
+        <div className={'px-6 text-sm font-semibold'}>Anywhere</div>
+        <div
+          className={
+            'hidden flex-1 border-x px-6 text-center text-sm font-semibold sm:block'
+          }
+        >
+          Any Week
+        </div>
+        <div
+          className={'flex items-center gap-3 pl-6 pr-2 text-sm text-gray-600'}
+        >
+          <div className={'hidden sm:block'}>Add Guests</div>
+          <div className={'rounded-full bg-rose-500 p-2 text-white'}>
+            <BiSearch role={'img'} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
