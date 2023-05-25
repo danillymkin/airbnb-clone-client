56ef0d0a04a41a22c7d82b652cfe737a58066d15
danillyamkin <danilwebdev@gmail.com>
2023-05-25 17:56:04 +0300

    Removed extra class

--- src/shared/ui/Container/Container.tsx
+++ src/shared/ui/Container/Container.tsx
@@ -10,9 +10,7 @@ export const Container: FunctionComponent<Props> = ({
   children,
 }): JSX.Element => {
   return (
-    <div
-      className={'lg: mx-auto max-w-[2520px] px-4 sm:px-2 md:px-10 xl:px-20'}
-    >
+    <div className={'mx-auto max-w-[2520px] px-4 sm:px-2 md:px-10 xl:px-20'}>
       {children}
     </div>
   );
