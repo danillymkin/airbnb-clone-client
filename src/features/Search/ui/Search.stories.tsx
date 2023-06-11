3e7994acc2a9adc0ef2d22a9c0df83dc19200f5f
danillyamkin <danilwebdev@gmail.com>
2023-06-12 02:51:22 +0300

    test(search): change import

--- src/features/Search/ui/Search.stories.tsx
+++ src/features/Search/ui/Search.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { Search } from './Search';
+import { Search } from '@/features/Search';
 
 const meta: Meta = {
   title: 'Features/Search',
