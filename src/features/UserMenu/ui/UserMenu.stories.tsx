d13030c60041eaca486185306ebf53bd0775fa5a
danillyamkin <danilwebdev@gmail.com>
2023-06-11 04:27:49 +0300

    test(user menu): create stories

---
+++ src/features/UserMenu/ui/UserMenu.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { UserMenu } from './UserMenu';
+
+const meta: Meta = {
+  title: 'Features/UserMenu',
+  component: UserMenu,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof UserMenu>;
+
+export const Default: Story = {
+  render: () => <UserMenu />,
+};
