91e77e5da18f507c390ced40cd74603a80e0010a
danillyamkin <danilwebdev@gmail.com>
2023-06-11 04:27:19 +0300

    test(user menu): create tests

---
+++ src/features/UserMenu/ui/UserMenu.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import { UserMenu } from '@/features/UserMenu';
+
+describe('User Menu', () => {
+  it('should render UserMenu', () => {
+    render(<UserMenu />);
+    const userMenu = screen.getByTestId('user-menu');
+    expect(userMenu).toBeInTheDocument();
+  });
+
+  it('should render your home button', () => {
+    render(<UserMenu />);
+    const yourHomeBtn = screen.getByText(/airbnb your home/i);
+    expect(yourHomeBtn).toBeInTheDocument();
+  });
+
+  it('should render menu', () => {
+    render(<UserMenu />);
+    const menu = screen.getByTestId('menu');
+    expect(menu).toBeInTheDocument();
+  });
+});
